8f2a29181ef9bc3a213a5fda473cda6b7fb9589d
camyacodes <camyacodes@gmail.com>
2021-11-02 15:04:46 -0400

    password length input and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = Number(
+    window.prompt(
+      "How long do you want the password to be? (between 8-128 characters)"
+    )
+  );
+
+  if (
+    passwordLength === "" ||
+    passwordLength === "null" ||
+    passwordLength > 128 ||
+    passwordLength < 8
+  ) {
+    window.alert("Please enter a value between 8 and 128");
+    return writePassword();
+  }
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
