432d6152da200b6fb70a5daa060fd8a00215870f
camyacodes <camyacodes@gmail.com>
2021-11-05 14:05:08 -0400

    added all the question prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,54 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var alpha = ["A", "B", "C", "D", "E", "F", "G"];
+
 // Write password to the #password input
 function writePassword() {
   //Create variable that captures users response to pw length
   var passwordLength = Number(
     window.prompt(
-      "How long do you want the password to be? (between 8-128 characters)"
+      "How long do you want the password to be? (Enter a number between 8-128 characters)"
     )
   );
-//Authenticates users answer to make sure its a number between 8-128
+  //Authenticates users answer to make sure its a number between 8-128
   if (
     passwordLength === "" ||
     passwordLength === "null" ||
     passwordLength > 128 ||
-    passwordLength < 8
+    passwordLength < 8 ||
+    Number.isNaN(passwordLength) === true
   ) {
     //reminds user of the parameters and then goes back to the original prompt
-    window.alert("Please enter a value between 8 and 128");
+    window.alert("Please enter a number between 8 and 128");
     return writePassword();
-  } else { 
-    //If response is valid, use it in creating an array of that length, which will become the pw
-    var password = Array.from({ length: passwordLength }, () =>
-      Math.floor(Math.random() * 100)
-    );
-    //variable that targets text box
-    var passwordText = document.querySelector("#password");
-    //displays the pw in the text box
-    passwordText.value = password;
   }
+  //Include NUMBERS?
+  var includeNumbers = window.prompt(
+    "Would you like your password to include NUMBERS? Enter YES or NO to continue."
+  );
+  //Include UPPERCASE letters?
+  var includeUpper = window.prompt(
+    "Would you like your password to include UPPERCASE letters? Enter YES or NO to continue."
+  );
+   //Include LOWERCASE letters?
+  var includeLower = window.prompt(
+    "Would you like your password to include LOWERCASE letters? Enter YES or NO to continue."
+  );
+   //Include SPECIAL CHARARCTERS?
+  var includeSpecial = window.prompt(
+    "Would you like your password to include SPECIAL CHARACTERS? Enter YES or NO to continue."
+  );
+  // else {
+    //If response is valid, use it in creating an array of that length, which will become the pw
+   // var password = passwordLength;
+    //variable that targets text box
+    //var passwordText = document.querySelector("#password");
+    //displays the pw in the text box
+    //passwordText.value = password;
+  //}
 
- // var password = generatePassword();
+  // var password = generatePassword();
 
   //var passwordText = document.querySelector("#password");
 
